076b54219fc6572a3cefce75e4aee9993a43378b
Bobby Nguyen <linh.ng754@gmail.com>
2019-04-16 19:51:40 -0400

    Styled homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
 import mainImage from './oldHickey.jpg';
 import './App.css';
 
@@ -9,16 +8,30 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
+      <body>
+        <div className="App">
 
-        <h1 class = "appTitle">BELP</h1>
-        <img
-         src={mainImage}
-         alt="profImage"
-         />
-         <h2 class = "appHeader">By Bonnies for Bonnies</h2>
-         <h3 class = "bobby">Designed by Bobby "StyleMaster" Nguyen</h3>
-      </div>
+          <div className = "flex-container">
+            <h1 className = "appTitle">BELP</h1>
+            <div className = "Buttons ">
+
+              <h2 className = "button"> Home</h2>
+              <h2 className = "button">About</h2>
+              <h2 className = "button">Contact Us</h2>
+            </div>
+
+          </div>
+
+          <p className = "LittleIntro">A Guide to Eat Out near St. Bonaventure</p>
+          <p className = "appHeader">By Bonnies for Bonnies</p>
+
+
+          </div>
+
+
+
+
+      </body>
 
     );
   }
