6abb84bcfdfe4f270eabbba60af70306511c0041
Bobby Nguyen <linh.ng754@gmail.com>
2019-04-22 21:28:28 -0400

    Styled ContactUs Component

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const ContactUs = () => (
-  <div>
-    <li><Link to='/'>Home</Link></li>
-    <h1>If you would like to reach out to us, here is our information:</h1>
+  <div className = "contactUsMore">
+    <h2><Link to='/' style = {{textDecoration: 'none', color: 'black', fontSize: 25, display: 'flex', borderStyle: 'solid', alignItem: 'flex-end', paddingBottom: '10px', paddingTop: '10px'}}>Home</Link></h2>
+    <p>If you would like to reach out to us, here is our information:</p>
+    <div className = "verticalLine"></div>
   </div>
 )
 
