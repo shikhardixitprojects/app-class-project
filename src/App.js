753d0d0dce408ecffb6d285c2befcf02e949b06a
Pickle <Bobbyboberz@gmail.com>
2019-04-24 16:42:25 -0400

    Added bobby and mike to contact info including bitmoji, name, and email

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom'
 import Routes from './Routes'
 
 class App extends Component {
-
-
   render() {
 
     return (
--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Banner from './Banner'
+import bitBobby from "../assets/bitBobby.jpg"
+import bitMike from "../assets/bitMike.JPG"
 
 const ContactUs = () => (
   <div>
     <Banner/>
     <h1>If you would like to reach out to us, here is our information:</h1>
+    <div className = "contact">
+
+  <img className="bobby" src={bitMike}/>
+    <p> Michael Czyzewski </p>
+    <p> czyzewm16@bonaventure.edu </p>
+    
+    <img className="bobby" src={bitBobby}/>
+      <p> Bobby Nguyen </p>
+      <p> nguyenla16@bonaventure.edu </p>
+
+
+
+    </div>
+
   </div>
 )
 
