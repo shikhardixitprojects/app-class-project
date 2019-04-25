cc6af9de2fe0f6f1d5d081ac1ccc49e0b3c9a5bd
Pickle <Bobbyboberz@gmail.com>
2019-04-25 00:59:04 -0400

    Added shiki and kat to contact with their info. Cut out the white in pic

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Banner from './Banner'
-import bitBobby from "../assets/bitBobby.jpg"
-import bitMike from "../assets/bitMike.JPG"
+import bitBobby from "../assets/bitBobby.png"
+import bitMike from "../assets/bitMike.png"
+import bitShiki from "../assets/bitShiki.png"
+import bitKat from "../assets/bitKat.png"
 
 const ContactUs = () => (
   <div>
@@ -10,15 +12,21 @@ const ContactUs = () => (
     <h1>If you would like to reach out to us, here is our information:</h1>
     <div className = "contact">
 
-  <img className="bobby" src={bitMike}/>
-    <p> Michael Czyzewski </p>
-    <p> czyzewm16@bonaventure.edu </p>
-    
+    <img className="bobby" src={bitMike}/>
+      <p> Michael Czyzewski </p>
+      <p> czyzewm16@bonaventure.edu </p>
+
     <img className="bobby" src={bitBobby}/>
       <p> Bobby Nguyen </p>
       <p> nguyenla16@bonaventure.edu </p>
 
+    <img className="bobby" src={bitShiki}/>
+      <p> Shiki Dixit </p>
+      <p> shikhardixit17@gmail.com </p>
 
+      <img className="bobby" src={bitKat}/>
+        <p> Katarina Jones </p>
+        <p> nguyenla16@bonaventure.edu </p>
 
     </div>
 
