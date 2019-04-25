c49c2b86dd3a5f94fc47ea119a45215d05889d14
Pickle <Bobbyboberz@gmail.com>
2019-04-25 14:13:51 -0400

    Added George. Fixed Kat's email.

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -5,6 +5,7 @@ import bitBobby from "../assets/bitBobby.png"
 import bitMike from "../assets/bitMike.png"
 import bitShiki from "../assets/bitShiki.png"
 import bitKat from "../assets/bitKat.png"
+import bitGeorge from "../assets/bitGeorge.png"
 
 const ContactUs = () => (
   <div>
@@ -12,28 +13,35 @@ const ContactUs = () => (
     <h1>If you would like to reach out to us, here is our information:</h1>
 
     <div className="contact">
-    <img className="bobby" src={bitMike}/>
+    <img className="bitImg" src={bitMike}/>
       <p> Michael Czyzewski </p>
       <p> czyzewm16@bonaventure.edu </p>
 </div>
 
   <div className="contact">
-    <img className="bobby" src={bitBobby}/>
+    <img className="bitImg" src={bitBobby}/>
       <p> Bobby Nguyen </p>
       <p> nguyenla16@bonaventure.edu </p>
 </div>
 
   <div className="contact">
-    <img className="bobby" src={bitShiki}/>
+    <img className="bitImg" src={bitShiki}/>
       <p> Shiki Dixit </p>
       <p> shikhardixit17@gmail.com </p>
 </div>
 
   <div className="contact">
-      <img className="bobby" src={bitKat}/>
+      <img className="bitImg" src={bitKat}/>
         <p> Katarina Jones </p>
-        <p> nguyenla16@bonaventure.edu </p>
+        <p> joneskn17@bonaventure.edu </p>
         </div>
+
+  <div className="contact">
+      <img className="bitImg" src={bitGeorge}/>
+        <p> George Gadelha</p>
+        <p> gadelhgj19@bonaventure.edu </p>
+        </div>
+
 
 
   </div>
