b19953cf7a3b49e066141a69fb4a4e5e86668359
Bobby Nguyen <linh.ng754@gmail.com>
2019-04-25 12:23:11 -0400

    Added title pitch to banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Banner = () => (
   <div className = "flex-container">
-    <h1 className = "appTitle" style = {{color: 'black'}}>BELP</h1>
+    <h1 className = "appTitle" style = {{color: 'black'}}>BELP:</h1>
+    <h2 className = "belpTitle">Bonnies'YELP</h2>
     <div className = "ButtonsSection ">
       <h2 className = "button"><Link to='/' style= {{textDecoration: 'none', color: 'black'}}>Home</Link></h2>
       <h2 className = "button"><Link to='/about' style= {{textDecoration: 'none', color: 'black'}}>About</Link></h2>
-      <h2 className = "button"><Link to='/ContactUs' style= {{textDecoration: 'none', color: 'black'}}>Contact Us</Link></h2>
+      <h2 className = "button"><Link to='/ContactUs' style= {{textDecoration: 'none', color: 'black'}}>Contact</Link></h2>
     </div>
   </div>
 )
