5c6a0d319fd6ec2eca58fc25e4e31224b5c97786
Shikhar <ShikharDixit@Shikis-MacBook-Air.local>
2019-04-25 14:20:47 -0400

    Took out unneeded Link imports

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Banner from './Banner'
 import bitBobby from "../assets/bitBobby.png"
 import bitMike from "../assets/bitMike.png"
