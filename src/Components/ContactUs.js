a9cb9b41a41875a474c4cc410ce4ad5243ea9365
Shikhar <ShikharDixit@Shikis-MacBook-Air.local>
2019-04-29 18:16:47 -0400

    made coding of contact us dynamic

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -6,41 +6,29 @@ import bitShiki from "../assets/bitShiki.png"
 import bitKat from "../assets/bitKat.png"
 import bitGeorge from "../assets/bitGeorge.png"
 
+const founders = [
+  { file : bitMike, name:"Michael Czyzewski", email: "czyzewm16@bonaventure.edu" },
+  { file : bitBobby, name:"Bobby Nguyen", email: "nguyenla16@bonaventure.edu" },
+  { file : bitShiki, name: "Shikhar Dixit", email:"shikhardixit17@gmail.com" },
+  { file : bitKat, name:"Katarina Jones", email: "joneskn17@bonaventure.edu" },
+  { file : bitGeorge, name: "George Gadelha", email:"gadelhgj19@bonaventure.edu" },
+]
+
 const ContactUs = () => (
   <div>
     <Banner/>
     <h2 className = "contactPitch">OUR FOUNDING MEMBERS:</h2>
-
-    <div className="contact">
-    <img className="bitImg" src={bitMike}/>
-      <p> Michael Czyzewski </p>
-      <p> czyzewm16@bonaventure.edu </p>
-</div>
-
-  <div className="contact">
-    <img className="bitImg" src={bitBobby}/>
-      <p> Bobby Nguyen </p>
-      <p> nguyenla16@bonaventure.edu </p>
+{
+founders.map( ({file, name, email}) => (
+  <div className = "contact">
+  <img key = {name} className = "bitImg" src = {file}/>
+    <p> {name} </p>
+    <p> {email} </p>
   </div>
-
-  <div className="contact">
-    <img className="bitImg" src={bitShiki}/>
-      <p> Shiki Dixit </p>
-      <p> shikhardixit17@gmail.com </p>
+))
+}
 </div>
-
-  <div className="contact">
-      <img className="bitImg" src={bitKat}/>
-        <p> Katarina Jones </p>
-        <p> joneskn17@bonaventure.edu </p>
-        </div>
-
-  <div className="contact">
-      <img className="bitImg" src={bitGeorge}/>
-        <p> George Gadelha</p>
-        <p> gadelhgj19@bonaventure.edu </p>
-        </div>
-  </div>
 )
+
 
 export default ContactUs
