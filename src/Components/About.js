7a06d707667c8dd7b2d9db14e57be4a4babd7f0c
Bobby Nguyen <linh.ng754@gmail.com>
2019-04-29 18:10:33 -0400

    semi-fixed the About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,11 +4,21 @@ import Banner from './Banner'
 const About = () => (
   <div>
     <Banner/>
-    <h2 className = "aboutPitch">Welcome to BELP, Bonnies' YELP page!
-    <brk></brk>
-    BELP was created to provide St. Bonaventure University students
-    extra information on where to eat out in town.
-    Our goal is to help YOU decide which restaurants fit your appetite, mood, and pocket.</h2>
+    <h2 className = "contactPitch">WELCOME TO BELP </h2>
+    <br/>
+    <h3 className = "aboutWhat">What is this?</h3>
+
+    <p className = "aboutIntro"> The creation of BELP started as an inspiration from YELP
+     and as a class project for IT280B - App Development. Our founding members wanted a streamlined
+     resource of restaurants and bars around town exlusively for St. Bonaventure students because as students ourselves,
+     we understand what we want and where we want to dine.</p>
+     <br/>
+       <h3 className = "aboutWhat">Our goal is:</h3>
+    <p className = "aboutIntro">We hope that with BELP, St. Bonaventure students
+    will have access to these resources to make the best choice based on appetite, style, and pocket.
+    Furthermore, we also want to encourage fellow students to really see the power of computer science
+    and how it enables the creation of amazing things (such as, this website).  
+    </p>
   </div>
 )
 
