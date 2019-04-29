a055feba8374b2377f7c09ca5770e306e370e4b3
Shikhar <ShikharDixit@Shikis-MacBook-Air.local>
2019-04-29 18:41:57 -0400

    Fixed errors in Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,24 +6,16 @@ import Banner from './Banner'
 class Home extends Component {
   render() {
     return (
-      <body>
-        <div className="App">
-
-          <Banner/>
-
-            <p className = "appHeader">Please click on an option to explore more</p>
-
-
-          </div>
-
-
-          <div className = "mainMenu">
-          <h2 className = "menuOptionsLocation"><Link to='/restaurants'
-                          style= {{textDecoration: 'none', color: 'black', paddingLeft: '15px'}}>RESTAURANTS</Link></h2>
+     <div className="App">
+     <Banner/>
+     <p className = "appHeader">Please click on an option to explore more</p>
+     <div className = "mainMenu">
+     <h2 className = "menuOptionsLocation"><Link to='/restaurants'
+                      style= {{textDecoration: 'none', color: 'black', paddingLeft: '15px'}}>RESTAURANTS</Link></h2>
           <div className = "verticalLine"></div>
           <h2 className = "menuOptionsLocation"><Link to='/bars' style= {{textDecoration: 'none', color: 'black',paddingRight: '125px'}}>BARS</Link></h2>
-          </div>
-      </body>
+      </div>
+      </div>
     );
   }
 }
