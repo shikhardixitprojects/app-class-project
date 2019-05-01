e27b26ce3a2cc758123d4bc84778e964a8c9bf41
Shikhar <ShikharDixit@Shikis-MacBook-Air.local>
2019-05-01 14:31:12 -0400

    added key to div element

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,8 +8,8 @@ const Nav = ({destinations, subroute}) => (
   <StickyBox style={{ height: '200px', border: '3px solid black',  width: '130px', backgroundColor: '#b8a9c9' }}>
   {
     destinations.map( ({headerText, name, route}) => (
-      <div className = "links">
-        <NavLink key = {name} exact to={"/" + subroute + "/" + route} style = {{textDecoration: 'none', color: 'black'}} >
+      <div className = "links" key = {name}>
+        <NavLink exact to={"/" + subroute + "/" + route} style = {{textDecoration: 'none', color: 'black'}} >
           {name}
         </NavLink>
       </div>
