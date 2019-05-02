98e83b0da67c01fe22e676deb35f213baa8d1d24
Shikhar <ShikharDixit@Shikis-MacBook-Air.local>
2019-05-02 11:17:19 -0400

    Added phone number

--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -6,9 +6,9 @@ import lakeview from "../assets/Lakeview.png"
 import dominos from "../assets/Dominoes.gif"
 
 const restaurants = [
-  { headerText : "Applebees: A Standard American Experience", name:"Applebees", route: "applebees", img: applebees, address: "3067 W State St, Olean, NY", deliver: "No delivery", review: "I personally enjoy the food here. Karaoke nights are always a plus" },
-  { headerText : "Lake View: Craving Chinese?", name:"Lake View", route: "lakeview", img: lakeview, address: "3042 NY-417, Olean, NY", deliver: "Yes delivery"},
-  { headerText : "Domino's: Yeah, we got 'em", name: "Domino's", route:"dominoes", img: dominos, address: "2626 W State St Unit 1, Olean, NY", deliver: "Yes, popular delivery" }
+  { headerText : "Applebees: A Standard American Experience", name:"Applebees", route: "applebees", img: applebees, address: "3067 W State St, Olean, NY", phoneNumber: "(716) 372-2828", deliver: "No delivery", review: "I personally enjoy the food here. Karaoke nights are always a plus" },
+  { headerText : "Lake View: Craving Chinese?", name:"Lake View", route: "lakeview", img: lakeview, address: "3042 NY-417, Olean, NY", phoneNumber: "(716) 373-0100", deliver: "Yes delivery"},
+  { headerText : "Domino's: Yeah, we got 'em", name: "Domino's", route:"dominoes", img: dominos, address: "2626 W State St Unit 1, Olean, NY", phoneNumber: "(716) 373-4210", deliver: "Yes, popular delivery" }
 ]
 
 const Restaurants = () => (
@@ -17,9 +17,9 @@ const Restaurants = () => (
           <Nav destinations = {restaurants} subroute = "restaurants"/>
       <div style={{ height: 800, border: '3px solid black'}}>
       {
-        restaurants.map( ({headerText, name, route, img, address, deliver, review}) => (
+        restaurants.map( ({headerText, name, route, img, address, phoneNumber, deliver, review}) => (
           <Route key={name} exact path={"/restaurants/" + route}
-          render = { () => <Restaurant headerText={headerText} img = {img} address = {address} deliver = {deliver} review = {review}/> } />
+          render = { () => <Restaurant headerText={headerText} img = {img} address = {address} phoneNumber = {phoneNumber} deliver = {deliver} review = {review}/> } />
         ))
       }
       </div>
@@ -28,14 +28,15 @@ const Restaurants = () => (
 
 )
 
-const Restaurant = ({ headerText, img, address, deliver, review }) => {
+const Restaurant = ({ headerText, img, address, phoneNumber, deliver, review }) => {
         return (
           <div className = "restaurant-page">
             <h1 style ={{fontFamily: 'Arial', letterSpacing: '3px', fontStyle: 'italic', fontSize: '25px'}}>{ headerText }</h1>
             <img className = "img" alt = {img} src = {img}/>
             <p style ={{fontFamily: 'Arial', letterSpacing: '5px', fontSize: '20px'}}>Address: { address }</p>
+            <p style ={{fontFamily: 'Arial', letterSpacing: '5px', fontSize: '20px'}}>Phone Number: { phoneNumber }</p>
             <p style ={{fontFamily: 'Arial', letterSpacing: '5px', fontSize: '20px'}}>{ deliver }
-        <br/> <br/> Review
+        <br/> <br/> Review:
             </p>
             <p style ={{fontFamily: 'Arial', letterSpacing: '2px', fontSize: '15px'}}>{review}</p>
           </div>
