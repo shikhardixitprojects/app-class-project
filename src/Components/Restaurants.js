34c1b9a31131ab84c741f8bd1e609282414ff62e
Shikhar <ShikharDixit@Shikis-MacBook-Air.local>
2019-05-03 13:19:10 -0400

    Added three restaurants

--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -4,11 +4,18 @@ import Nav from './Nav'
 import applebees from "../assets/Applebees.png"
 import lakeview from "../assets/Lakeview.png"
 import dominos from "../assets/Dominoes.gif"
+import rafis from "../assets/Rafi's Platter.jpg"
+import uniontea from "../assets/uniontea.jpeg"
+import beefnbarrel from "../assets/beef n barrel.jpg"
+
 
 const restaurants = [
-  { headerText : "Applebees: A Standard American Experience", name:"Applebees", route: "applebees", img: applebees, address: "3067 W State St, Olean, NY", phoneNumber: "(716) 372-2828", deliver: "No delivery", review: "I personally enjoy the food here. Karaoke nights are always a plus" },
+  { headerText : "Applebees: A Standard American Experience", name:"Applebees", route: "applebees", img: applebees, address: "3067 W State St, Olean, NY", phoneNumber: "(716) 372-2828", deliver: "No delivery", review: "Bobby Nguyen '21': I personally enjoy the food here. Karaoke nights are always a plus!" },
   { headerText : "Lake View: Craving Chinese?", name:"Lake View", route: "lakeview", img: lakeview, address: "3042 NY-417, Olean, NY", phoneNumber: "(716) 373-0100", deliver: "Yes delivery"},
-  { headerText : "Domino's: Yeah, we got 'em", name: "Domino's", route:"dominoes", img: dominos, address: "2626 W State St Unit 1, Olean, NY", phoneNumber: "(716) 373-4210", deliver: "Yes, popular delivery" }
+  { headerText : "Domino's: Yeah, we got 'em", name: "Domino's", route:"dominoes", img: dominos, address: "2626 W State St Unit 1, Olean, NY", phoneNumber: "(716) 373-4210", deliver: "Yes, popular delivery" },
+  { headerText : "Rafi's Platter: A Cultural Bliss", name: "Rafi's Platter", route:"rafiplatter", img: rafis, address: "800 Wayne St, Olean, NY 14760", phoneNumber: "(716) 790-8294", deliver: ""},
+  { headerText : "Union Tea Cafe: Pass the Tea!", name: "Union Tea Cafe", route:"unionteacafe", img: uniontea, address: "301 N Union St, Olean, NY 14760", phoneNumber: "(716) 701-4014", deliver: ""},
+  { headerText : "Beef'n Barrel: Local Favorite", name: "Beef'n Barrel", route:"beefnbarrel", img: beefnbarrel, address: "146 N Union St, Olean, NY 14760", phoneNumber: "(716) 372-2985", deliver: ""}
 ]
 
 const Restaurants = () => (
